5e049ca64f982af990decca451727917ab5fdb72
David Sanders <davesque@gmail.com>
2018-06-06 19:23:20 -0600

    Bump version: 0.1.0-alpha.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extras_require['dev'] = (
 setup(
     name='eth-typing',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.0-alpha.0',
+    version='0.2.0',
     description="""eth-typing: Common type annotations for ethereum python packages""",
     long_description_markdown_filename='README.md',
     author='The eth-typing contributors',
