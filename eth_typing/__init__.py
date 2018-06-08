006782704278ca455722c8b124d1a5bea19428ea
David Sanders <davesque@gmail.com>
2018-06-07 18:40:08 -0600

    Add py-evm resources

---
+++ eth_typing/__init__.py
@@ -0,0 +1,9 @@
+from .enums import (  # noqa: F401
+    ForkName
+)
+
+from .misc import (  # noqa: F401
+    Address,
+    BlockNumber,
+    Hash32,
+)
---
+++ eth_typing/misc.py
@@ -0,0 +1,7 @@
+from typing import (
+    NewType,
+)
+
+Address = NewType('Address', bytes)
+Hash32 = NewType('Hash32', bytes)
+BlockNumber = NewType('BlockNumber', int)
