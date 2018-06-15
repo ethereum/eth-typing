3c6803895bb2d36a9bda39e7875884751a109ce8
Nick Gheorghita <nickgheorghita@gmail.com>
2018-06-15 10:33:02 -0600

    Add types here from eth-utils

--- eth_typing/misc.py
+++ eth_typing/misc.py
@@ -1,7 +1,16 @@
 from typing import (
     NewType,
+    TypeVar,
+    Union,
 )
 
-Address = NewType('Address', bytes)
-Hash32 = NewType('Hash32', bytes)
+Address = NewType('Address', bytes)  # for canonical Addresses
 BlockNumber = NewType('BlockNumber', int)
+Hash32 = NewType('Hash32', bytes)
+HexAddress = NewType('HexAddress', str)  # for hex encoded addresses
+HexStr = NewType('HexStr', str)
+Primitives = Union[bytes, int, bool]  # for conversion utils
+
+ChecksumAddress = NewType('ChecksumAddress', HexAddress)  # for hex addresses with checksums
+
+AnyAddress = TypeVar('AnyAddress', Address, HexAddress, ChecksumAddress)
