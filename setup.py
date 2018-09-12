b2fbd48d469fa6bf230ab8c2cd506e232336ea7d
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2018-09-12 10:47:13 +0200

    Enable discovery of type hints as per PEP561

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
     zip_safe=False,
     keywords='ethereum',
     packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={'eth_typing': ['py.typed']},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
