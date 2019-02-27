c5b1b186925c0e190018a63a353b486c0375d56e
Chih Cheng Liang <chihchengliang@gmail.com>
2019-02-27 23:41:57 +0800

    Add BLS types
    ignore .pytest_cache
    
    fix lint

--- eth_typing/__init__.py
+++ eth_typing/__init__.py
@@ -2,11 +2,22 @@ from .abi import (  # noqa: F401
     Decodable,
     TypeStr,
 )
-
+from .bls import (  # noqa: F401
+    BLSPubkey,
+    BLSSignature,
+)
+from .encoding import (  # noqa: F401
+    HexStr,
+    Primitives,
+)
 from .enums import (  # noqa: F401
     ForkName,
 )
-
+from .ethpm import (  # noqa: F401
+    URI,
+    ContractName,
+    Manifest,
+)
 from .evm import (  # noqa: F401
     Address,
     AnyAddress,
@@ -15,15 +26,4 @@ from .evm import (  # noqa: F401
     ChecksumAddress,
     Hash32,
     HexAddress,
-)
-
-from .encoding import (  # noqa: F401
-    HexStr,
-    Primitives,
-)
-
-from .ethpm import (  # noqa: F401
-    ContractName,
-    Manifest,
-    URI,
 )
