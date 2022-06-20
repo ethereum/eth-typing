2dacf9f81bc1d80f017727998a612515ffdad929
Felipe Selmo <fselmo2@gmail.com>
2022-06-20 13:00:42 -0600

    Updates to modernize ``towncrier`` inclusion PR
    * Update towncrier version in ``setup.py``
    * Update Makefile
    * Update /newsfragments' supporting files with the updated setup from the `ethereum-python-project-template` repo
    * Update `tox.ini` and CI's `config.yaml`
    * Update `pyproject.toml`

--- newsfragments/validate_files.py
+++ newsfragments/validate_files.py
@@ -3,15 +3,18 @@
 # Towncrier silently ignores files that do not match the expected ending.
 # We use this script to ensure we catch these as errors in CI.
 
-import os
 import pathlib
+import sys
 
 ALLOWED_EXTENSIONS = {
-    '.feature.rst',
+    '.breaking.rst',
     '.bugfix.rst',
     '.doc.rst',
-    '.removal.rst',
+    '.feature.rst',
+    '.internal.rst',
     '.misc.rst',
+    '.performance.rst',
+    '.removal.rst',
 }
 
 ALLOWED_FILES = {
@@ -21,11 +24,22 @@ ALLOWED_FILES = {
 
 THIS_DIR = pathlib.Path(__file__).parent
 
+num_args = len(sys.argv) - 1
+assert num_args in {0, 1}
+if num_args == 1:
+    assert sys.argv[1] in ('is-empty', )
+
 for fragment_file in THIS_DIR.iterdir():
 
     if fragment_file.name in ALLOWED_FILES:
         continue
-
-    full_extension = "".join(fragment_file.suffixes)
-    if full_extension not in ALLOWED_EXTENSIONS:
-        raise Exception(f"Unexpected file: {fragment_file}") 
+    elif num_args == 0:
+        full_extension = "".join(fragment_file.suffixes)
+        if full_extension not in ALLOWED_EXTENSIONS:
+            raise Exception(f"Unexpected file: {fragment_file}")
+    elif sys.argv[1] == 'is-empty':
+        raise Exception(f"Unexpected file: {fragment_file}")
+    else:
+        raise RuntimeError(
+            "Strange: arguments {sys.argv} were validated, but not found"
+        )
