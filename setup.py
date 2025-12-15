a9f1ace9116e7492be6240667ec7de4cd79ab41f
kclowes <kclowes@users.noreply.github.com>
2025-12-15 10:45:21 -0700

    Bump version: 5.2.1 → 6.0.0-beta.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     name="eth-typing",
     # *IMPORTANT*: Don't manually change the version here.
     # See Contributing docs for the release process.
-    version="5.2.1",
+    version="6.0.0-beta.1",
     description="""eth-typing: Common type annotations for ethereum python packages""",
     long_description=long_description,
     long_description_content_type="text/markdown",
